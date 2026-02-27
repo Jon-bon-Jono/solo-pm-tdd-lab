d52fcf78a6b0d615b67392f2c0d4265af862ec34
Jon-bon-jono <z5162987@student.unsw.edu.au~>
2026-02-28 09:52:09 +1100

    feat: broke the function.

--- src/solo_pm_tdd_lab/core.py
+++ src/solo_pm_tdd_lab/core.py
@@ -1,4 +1,5 @@
 def clamp(x: float, lo: float, hi: float) -> float:
     """Clamp x to [lo, hi]."""
     if lo > hi: raise ValueError("In clamp lo must be < hi.")
-    return max(lo, min(x, hi))
+    return None
+    #return max(lo, min(x, hi))
